603d2d663713fcc5a429a400fe6ac9e0a4c7b26f
Saul Gonzalez <gonzalezsaul31@gmail.com>
2025-03-14 18:20:02 -0600

    refactor, it works but cant get cookies to comply well

--- yt-dlp.py
+++ yt-dlp.py
@@ -1,4 +1,7 @@
 import subprocess
+import browser_cookie3 as bc
+import tempfile
+from urllib.parse import urlparse
 
 
 def get_video_url(choice):
@@ -32,7 +35,75 @@ def grab_supported_browser():
         )
 
 
-def download_video(url, browser, is_playlist=False):
+def get_domain(url):
+    try:
+        parsed_url = urlparse(url)
+        domain = parsed_url.netloc
+        return domain
+    except Exception as e:
+        print(f"ERROR: failed to extract domain from URL: {e}")
+        return None
+
+
+# this function can be refactored
+def get_cookies(browser, domain):
+    try:
+        match browser.lower().strip():
+            case "chrome":
+                cookies = bc.chrome()
+            case "brave":
+                print("found brave!!")
+                cookies = bc.brave()
+            case "edge":
+                cookies = bc.edge()
+            case "firefox":
+                cookies = bc.firefox()
+            case "operagx":
+                cookies = bc.opera_gx()
+            case "vivaldi":
+                cookies = bc.vivaldi()
+            case "safari":
+                cookies = bc.safari()
+            case "w3m":
+                cookies = bc.w3m()
+            case "lynx":
+                cookies = bc.lynx()
+            case "opera":
+                cookies = bc.opera()
+            case "librewolf":
+                cookies = bc.librewolf()
+            case _:
+                print("Unsupported browser, defaulting to no authentication!")
+                return None
+
+        cookie_str = ""
+        normailized_domain = domain.lstrip("www.")
+        cookie_file = tempfile.NamedTemporaryFile(
+            delete=True,
+            mode="w",
+            newline="",
+            dir="./",
+            prefix="cookies_",
+            suffix=".txt",
+        )
+        cookie_file_path = cookie_file.name
+
+        for cookie in cookies:
+            cookie_domain = cookie.domain.lstrip("www.")
+            if normailized_domain in cookie_domain:
+                cookie_str += f"{cookie.name}={cookie.value}"
+
+        cookie_file.write(cookie_str)
+        cookie_file.close()
+
+        return cookie_file_path
+
+    except Exception as e:
+        print(f"ERROR: failed to extract cookies {e}")
+        return None
+
+
+def download_video(url, browser, cookies, is_playlist=False):
     base_command = [
         "yt-dlp",
         "-f",
@@ -41,8 +112,9 @@ def download_video(url, browser, is_playlist=False):
         "mp4",
     ]
 
-    if browser:
-        base_command.extend(["--cookies-from-browser", browser])
+    if browser and cookies:
+        base_command.extend(["--cookies", cookies])
+
     if is_playlist:
         output_format = "%(playlist_index)s - %(title)s.%(ext)s"
     else:
@@ -51,6 +123,7 @@ def download_video(url, browser, is_playlist=False):
     base_command.extend(["-o", output_format])
     base_command.append(url)
 
+    print(base_command)
     subprocess.run(base_command)
 
 
@@ -69,12 +142,16 @@ def main():
         if choice == "1":
             url = get_video_url(choice)
             browser = grab_supported_browser()
-            download_video(url, browser, is_playlist=False)
+            domain = get_domain(url)
+            cookies = get_cookies(browser, domain)
+            download_video(url, browser, cookies, is_playlist=False)
             break
         elif choice == "2":
             url = get_video_url(choice)
             browser = grab_supported_browser()
-            download_video(url, browser, is_playlist=True)
+            domain = get_domain(url)
+            cookies = get_cookies(browser, domain)
+            download_video(url, browser, cookies, is_playlist=True)
             break
         elif choice == "3":
             print("exiting script, good bye!")
